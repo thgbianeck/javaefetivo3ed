1d1f2a6949121609aa4172fb177d16a04cd31aaf
thgbianeck <thgbianeck@gmail.com>
2025-06-18 10:18:50 -0300

    Commit Inicial

---
+++ capitulo02/item01/src/test/java/br/com/bianeck/javaefetivo/capitulo02/item01/MainTest.java
@@ -0,0 +1,7 @@
+package br.com.bianeck.javaefetivo.capitulo02.item01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
---
+++ commons/src/test/java/br/com/bianeck/javaefetivo/commons/MainTest.java
@@ -0,0 +1,7 @@
+package br.com.bianeck.javaefetivo.commons;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
